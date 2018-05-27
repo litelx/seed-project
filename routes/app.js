dae3689f73181ce3848c4f00d62d2cac550efce8
Lital <lital.jos@gmail.com>
2018-05-27 12:28:23 +0300

    Added templates and styles

--- routes/app.js
+++ routes/app.js
@@ -1,24 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
+// var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('node');
+    res.render('index');
 });
+// router.get('/', function (req, res, next) {
+//     res.render('node');
+// });
 
-router.post('/', function (req, res, next) {
+// router.post('/', function (req, res, next) {
     
-    var user = new User({
-        firstName: 'litel',
-        lastName: 'josif',
-        password: 'super-secret',
-        email: req.body.email,
-    })
+//     var user = new User({
+//         firstName: 'litel',
+//         lastName: 'josif',
+//         password: 'super-secret',
+//         email: req.body.email,
+//     })
     
-    user.save()
-    console.log('saved', user)
+//     user.save()
+//     console.log('saved', user)
     
-    res.redirect('/')
-});
+//     res.redirect('/')
+// });
 
 module.exports = router;
