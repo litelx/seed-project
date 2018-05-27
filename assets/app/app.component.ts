567446bb8bfe3db7fd48b83b739ac00da88c6840
Lital <lital.jos@gmail.com>
2018-05-27 16:42:06 +0300

    Added components and data binding

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,28 +1,10 @@
 import { Component } from '@angular/core';
+import { Message } from './messages/message.model';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html',
-    styles: [`
-        .author {
-            display: inline-block;
-            font-style: italic;
-            font-size: 12px;
-            width: 80%;
-        }
-        .config {
-            display: inline-block;
-            text-align: right;
-            font-size: 12px;
-            width: 19%;            
-        }
-    `]
-
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-    message = {
-        content: 'A message',
-        author: 'Litel',
-
-    }
+    message: Message = new Message('A message', 'Litel')
 }
