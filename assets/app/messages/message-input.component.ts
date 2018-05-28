a18dda3a0c29675a2abe991862ccb5093c41d12d
Lital <lital.jos@gmail.com>
2018-05-28 20:00:34 +0300

    Add MessageService service

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MessageService } from './message.service';
+import { Message } from './message.model';
 
 @Component({
   selector: 'app-message-input',
-  templateUrl: './message-input.component.html'
+  templateUrl: './message-input.component.html',
+  providers: [MessageService]
 })
 
 export class MessageInputComponent {
-  onSave(value: string) {
+  constructor(private messageService: MessageService) {
+  }
 
+  onSave(value: string) {
+      const message = new Message(value, 'litel')
+      this.messageService.addMessage(message)
   }
 }
