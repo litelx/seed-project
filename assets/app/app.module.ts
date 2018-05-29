3b14250926267711d3590c68ee73b376fe311d8d
Lital <lital.jos@gmail.com>
2018-05-29 21:59:24 +0300

    Creating the header and re-structing project

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from "./app.component";
 import { MessageComponent } from "./messages/message.component";
 import { MessageListComponent } from "./messages/message-list.component";
 import { MessageInputComponent } from "./messages/message-input.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { AuthanticationComponent } from './auth/authantication.component';
+import { HeaderComponent } from './header.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         MessageComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        MessagesComponent,
+        AuthanticationComponent,
+        HeaderComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
---
+++ assets/app/messages/messages.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-messages',
+    templateUrl: './messages.Component.html'
+})
+
+export class MessagesComponent {
+
+}
